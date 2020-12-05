b6b8fe2bd2c0b20b4b2227b9d8912e49ce8c6404
zlli3 <zlli3@chinaums.com>
2020-12-05 20:18:34 +0800

    Initial Commit

---
+++ iOSCollector/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSCollector
+//
+//  Created by lizhonglian on 2020/12/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSCollector/iOSCollectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSCollectorApp.swift
+//  iOSCollector
+//
+//  Created by lizhonglian on 2020/12/5.
+//
+
+import SwiftUI
+
+@main
+struct iOSCollectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
