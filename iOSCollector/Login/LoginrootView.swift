e39d9921fc562953abd24ea03d235b9611216652
zlli3 <zlli3@chinaums.com>
2020-12-06 10:25:41 +0800

    plugin

--- iOSCollector/Login/LoginrootView.swift
+++ iOSCollector/Login/LoginrootView.swift
@@ -8,12 +8,12 @@
 import SwiftUI
 
 struct LoginrootView: View {
-    
+
     @ObservedObject var login = IsLogin()
-    
+
     var body: some View {
         VStack{
-            
+
             // 当isLogin 被置成true时，用到isLogin的页面都会刷新；因此这里会进行判断，显示哪个view
             if self.login.isLogin == false{
                 LoginFirstView(loginButton: self.login)
