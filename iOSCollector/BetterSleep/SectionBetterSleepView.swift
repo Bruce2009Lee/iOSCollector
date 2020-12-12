b1517fb389f7b840a71982c970878dc0e6899fad
zlli3 <zlli3@chinaums.com>
2020-12-12 19:43:20 +0800

    完成betterSleep 挑战

--- iOSCollector/BetterSleep/SectionBetterSleepView.swift
+++ iOSCollector/BetterSleep/SectionBetterSleepView.swift
@@ -18,6 +18,27 @@ struct SectionBetterSleepView: View {
     @State private var alertMessage = ""
     @State private var showingAlert = false
     
+    private var bedTime:String {
+        let model = SleepCalculator()
+        
+        let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+        let hour = (components.hour ?? 0) * 60 * 60
+        let minute = (components.minute ?? 0) * 60
+        
+        do {
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            let formatter = DateFormatter()
+            formatter.timeStyle = .short
+            
+            return formatter.string(from: sleepTime)
+        } catch {
+            return ""
+        }
+    }
+    
     private static var defaultWakeTime: Date {
         var components = DateComponents()
         components.hour = 7
@@ -52,33 +73,32 @@ struct SectionBetterSleepView: View {
     var body: some View {
         NavigationView {
             Form {
-                Text("When do you want to wake up?")
-                    .font(.headline)
+                Section(header: Text("When do you want to wake up?").font(.subheadline)) {
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                        .datePickerStyle(WheelDatePickerStyle())
+                }
                 
-                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                    .labelsHidden()
-                    .datePickerStyle(WheelDatePickerStyle())
-                
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Desired amount of sleep")
-                        .font(.headline)
-                    
+                Section(header: Text("Desired amount of sleep").font(.subheadline)){
                     Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
                         Text("\(sleepAmount, specifier: "%g") hours")
                     }
                 }
                 
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Daily coffee intake")
-                        .font(.headline)
-                    
-                    Stepper(value: $coffeeAmount, in: 1...20) {
-                        if coffeeAmount == 1 {
-                            Text("1 cup")
-                        } else {
-                            Text("\(coffeeAmount) cups")
+                Section(header: Text("Daily coffee intake").font(.headline)){
+                    Picker("cpu(s) of coffee", selection: $coffeeAmount) {
+                        ForEach(1 ..< 10) {
+                            if $0 == 1 {
+                                Text("1  cup")
+                            } else {
+                                Text("\($0)  cups")
+                            }
                         }
                     }
+                }
+                
+                Section(){
+                    Text("Your ideal bedtime is \(bedTime)")
                 }
                 
             }
